ca86e0b210f46871f584b9540a4e18ea4e63e48f
Justin Ndwiga <ndwigajustin@gmail.com>
2018-03-02 12:41:25 +0300

    Add Comment module

---
+++ src/comment.py
@@ -0,0 +1,16 @@
+from user import User
+
+class Comment:
+    def __init__(self):
+        pass
+
+    def add_comment(self):
+        pass
+
+    def edit_comment(self):
+        pass
+
+    def delete_comment(self):
+        pass
+        
+    
--- src/user.py
+++ src/user.py
@@ -1,7 +1,8 @@
+
 class User:
     count = 1
     #users = [{"id":1,"username": "john","password":"123","role":"normal","comments":[]}]
-    def __init__(self, username, password, role, id):
+    def __init__(self, username, password, role, id=None):
         self.user=[]
         self.username = username
         self.password = password
@@ -12,16 +13,22 @@ class User:
 
 
 class Admin(User):
-    def __init__(self, username, password, role,id):
+    def __init__(self, username, password, role,id=None):
         super(User,self).__init__(self,id,username,password,role="admin")
 
     
     def create_user(self,username,password):
         new_user =dict(username=username,password=password)
         self.user.append(new_user)
+        return "Created user Successfully"
 
     # def login(self,username, password):
     #    if self.user[username] = username:
     #         return "Login success"
-        
     
+class Moderator(User):
+    pass
+
+
+a = User("a","b","c")
+print(a)
